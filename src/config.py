35cdc4df1511696df5678a5d7517fe38d1af06c9
amanuel <amanuelchukala@gmail.com>
2025-02-26 16:54:23 -0700

    Add database configuration settings and environment variable loading in config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,14 @@
-# Database configuration settings
+import pandas as pd
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+DATASET_NAME = "karkavelrajaj/amazon-sales-dataset"
+SAVE_DIRECTORY = Path("data/raw")
+CURRENT_DATE = pd.Timestamp.now().strftime('%Y_%m_%d')
+CSV_FILE_PATH = SAVE_DIRECTORY / f"amazon_{CURRENT_DATE}.csv"
+CONNECTION_STRING = os.getenv('DB_CONNECTION_STRING')
+TABLE_NAME = "amazon_sales_data"
+# Add more configuration variables as needed
