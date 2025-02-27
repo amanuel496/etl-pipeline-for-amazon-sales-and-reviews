901198cced28f7be43f6df44fad63284d532cc45
amanuel <amanuelchukala@gmail.com>
2025-02-27 15:42:16 -0700

    Add environment variable checks for database connection and email settings in config.py

--- src/config.py
+++ src/config.py
@@ -11,6 +11,23 @@ DATASET_NAME = "karkavelrajaj/amazon-sales-dataset"
 SAVE_DIRECTORY = Path("data/raw")
 CURRENT_DATE = pd.Timestamp.now().strftime('%Y_%m_%d')
 CSV_FILE_PATH = SAVE_DIRECTORY / f"amazon_{CURRENT_DATE}.csv"
+
 CONNECTION_STRING = os.getenv('DB_CONNECTION_STRING')
+if CONNECTION_STRING is None:
+    raise ValueError("DB_CONNECTION_STRING is not set in the environment variables.")
 TABLE_NAME = "amazon_sales_data"
+
+APP_PASSWORD = os.getenv('APP_PASSWORD')  # Email password for sending alerts
+if APP_PASSWORD is None:
+    raise ValueError("APP_PASSWORD is not set in the environment variables.")
+
+SENDER_EMAIL = os.getenv('SENDER_EMAIL')  # Sender email address for sending alerts
+if SENDER_EMAIL is None:
+    raise ValueError("SENDER_EMAIL is not set in the environment variables.")
+
+RECEIVER_EMAIL = os.getenv('RECEIVER_EMAIL')  # Receiver email address for sending alerts
+if RECEIVER_EMAIL is None:
+    raise ValueError("RECEIVER_EMAIL is not set in the environment variables.")
+
+SUBJECT = "ETL Pipeline Failed 🚨"
 # Add more configuration variables as needed
